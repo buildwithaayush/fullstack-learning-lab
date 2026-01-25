f3ee05f8196d59f7dfb43eb1fb83e558a9986470
buildwithaayush <ayushxdev347@gmail.com>
2026-01-25 20:36:32 +0530

    setup prisma but prismaclient issue persist

--- nextjs/week-19/app/api/v1/signup/route.ts
+++ nextjs/week-19/app/api/v1/signup/route.ts
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@/generated/prisma";
+const prismaClient = new PrismaClient();
 
 export async function POST(req: NextRequest){
     const data = await req.json();
